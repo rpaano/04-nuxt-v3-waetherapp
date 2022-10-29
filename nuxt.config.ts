a0d42eb7371416b942a18505dcdfc774684fd0c0
Romel Niño Paano <50399801+rpaano@users.noreply.github.com>
2022-10-30 01:33:06 +0800

    Update weather app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    publicRuntimeConfig: {
+        //Available for Server and Client
+        WEATHER_APP_SECRET: process.env.WEATHER_APP_SECRET,
+    },
+    privateRuntimeConfig: {
+        //Only available in Server
+    },
+    css: ["~/assets/css/tailwind.css"],
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
+    },
 })
